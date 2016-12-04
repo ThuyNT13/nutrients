6f076c2159a4c78b9d71ce12f4a68f4e03cf3a09
[01;31m[K"name"[m[K: "ThuyNT13 <[01;31m[K"email"[m[K: null>
2016-12-04 16:50:07 -0600

    add User routing specs, pass all but show

---
+++ spec/routing/user_routing_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "User routing", type: :routing do
+
+  it "routes GET /users/1 to profile" do
+    expect(:get => "/users/1").to route_to("users#show", :user_id => "1")
+  end
+  it "routes GET /users/new to users#new" do
+    expect(:get => "/users/new").to route_to("users#new")
+  end
+  it "routes POST /users to users#create"  do
+    expect(:post => "/users").to route_to("users#create")
+  end
+  it "routes GET /users/1/edit to users#edit" do
+    expect(:get => "/users/1/edit").to route_to("users#edit", :id => "1")
+  end
+  it "routes PUT /users/1 to users#update" do
+    expect(:put => "/users/1").to route_to("users#update", :id => "1")
+  end
+  it "routes DELETE /users/1 to users#destroy" do
+    expect(:delete => "/users/1").to route_to("users#destroy", :id => "1")
+  end
+
+end
